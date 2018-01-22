8a1faa2490b8ff904a8fc0bc9488c64cddb61ba0
Inna_Sokolenko <sokolenkoinna5@gmail.com>
2018-01-22 16:11:50 +0200

    Start App

---
+++ components/MySecondScreen.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  Image
+} from 'react-native';
+
+export default class MySecondScreen extends Component {
+  static navigationOptions = {
+    drawerLabel: 'Second',
+    drawerIcon: ({ tintColor }) => (
+      <Image
+        source={require('https://www.google.com.ua/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjFsPiu2evYAhXCZFAKHdYUCAkQjRwIBw&url=http%3A%2F%2Fbipbap.ru%2Fcategory%2Fkrasivye-kartinki&psig=AOvVaw1366rTdMaygtQeCwQQbPP2&ust=1516714880174299')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Соколенко
+        </Text>
+        <Button
+          onPress={() => this.props.navigation.navigate('Home')}
+          title="Go to home page"
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c4977',
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  }
+});
